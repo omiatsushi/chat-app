06cc728a1da7bf246c08e095650ef965fe9eebd6
omiatsushi <73770109+omiatsushi@users.noreply.github.com>
2020-12-06 17:24:59 +0900

    チャットルーム作成機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ApplicationRecord
 
          validates :name, presence: true
 
-         has_many :rooms, through: :user_rooms
+         
          has_many :room_users
+         has_many :rooms, through: :room_users
 
 end
